6f167e361f6244d441d6784d7aee20ec37b1d0bd
Lenisaliana <lenisasimangunsong@gmail.com>
2021-08-27 22:04:20 +0700

    My Fix Screen 1

---
+++ main.js
@@ -0,0 +1,15 @@
+function ph4() {
+    document.getElementById('changeImage').innerHTML='<img src="img/ph4.png">';
+}    
+function ph5() {
+    document.getElementById('changeImage').innerHTML='<img src="img/ph5.png">';
+} 
+function ph6() {
+    document.getElementById('changeImage').innerHTML='<img src="img/ph6.png">';
+} 
+function ph7() {
+    document.getElementById('changeImage').innerHTML='<img src="img/ph7.png">';
+} 
+function ph8() {
+    document.getElementById('changeImage').innerHTML='<img src="img/ph8.png">';
+} 
